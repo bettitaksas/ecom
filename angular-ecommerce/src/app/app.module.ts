7acb8675e2372c8935579a2c840fec2af910b713
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2024-01-18 13:17:46 +0100

    display the products with name and price

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductService} from "./services/product.service";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.ecom.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "product", path = "products")
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository  extends JpaRepository<Product,Long> {
 }
