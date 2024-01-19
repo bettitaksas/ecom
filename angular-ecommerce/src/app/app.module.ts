be5103ca43bb51937e7f23cafa11b4a30113e77f
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2024-01-19 12:09:16 +0100

    define routes

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -7,6 +7,15 @@ import {ProductListComponent} from './components/product-list/product-list.compo
 import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
 import {CommonModule} from "@angular/common";
+import {Routes, RouterModule} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -14,6 +23,7 @@ import {CommonModule} from "@angular/common";
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule,
     BrowserModule,
     AppRoutingModule,
